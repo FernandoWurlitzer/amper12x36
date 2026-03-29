e48e5ebc2b98db89a960dbc203598aa8ce9d9afe
Fernando Wurlitzer <wurlitzerfernando@gmail.com>
2026-03-29 15:53:54 +0000

    troque a fonte do cabeçalho por essa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,9 +24,9 @@ export function Header() {
             />
           </div>
           <div className="flex flex-col">
-            <h1 className="text-xl md:text-2xl tracking-tight text-foreground leading-none flex items-start">
-              <span className="font-semibold uppercase">AMPERNET</span>
-              <span className="text-[10px] md:text-xs font-medium lowercase ml-0.5 mt-0.5 opacity-80">12x36</span>
+            <h1 className="text-xl md:text-2xl tracking-tighter text-foreground leading-none flex items-start font-michroma lowercase">
+              <span className="font-normal">ampernet</span>
+              <span className="text-[10px] md:text-xs font-medium ml-0.5 mt-0.5 opacity-80 font-body align-top">12x36</span>
             </h1>
           </div>
         </div>
