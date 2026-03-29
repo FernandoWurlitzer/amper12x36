9580b9e3d3351ed2da26f3ef1d5a55e359f48b4b
Fernando Wurlitzer <wurlitzerfernando@gmail.com>
2026-03-29 13:38:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1794556324-c19f3",
+  "appId": "1:419955509236:web:7ae080f5de93ad6015dba1",
+  "apiKey": "AIzaSyAYKwn_aUV-_rjvqEhO8-DF-WPrmG4Hgr8",
+  "authDomain": "studio-1794556324-c19f3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "419955509236"
+};
